e95613ee3ed9b0e08a45f2bad9753fdd81cfe79b
akansha-singh <singhakansha281@gmail.com>
2020-05-10 14:14:06 +0530

    outputMessage

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000 || process.env.PORT;
+// Run when client connects
+io.on('connection', socket => {
+     
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    // Welcome current user
+    socket.emit('message', 'Welcome to ChatBot');
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined the channel');
+
+    // Runs when client disconnect
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the channel');
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', msg);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
